92ea5923d58baf11216b240533a8106cbad80ffa
paulomarinato <paulomarinato1@gmail.com>
2024-05-04 18:08:28 -0300

    Adicionando modificadores de layout

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.alcoolougasolina
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -30,7 +33,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
-    Text(text = "Álcool ou Gasolina")
-    Text(text = "Gasolina")
-
+    Box {
+        Text(text = "Álcool ou Gasolina")
+        Text(text = "Gasolina")
+    }
+    Column {
+        Text(text = "Álcool ou Gasolina")
+        Text(text = "Gasolina")
+    }
+    Row {
+        Text(text = "Álcool ou Gasolina")
+        Text(text = "Gasolina")
+    }
 }
